773e1246ecb581b7b4dbf81177cffe23770531fa
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-09-09 12:47:15 +0200

    simplify x-and-ride app and test

--- e2e/topicmaps/x-and-ride/src/e2e/smoke.spec.ts
+++ e2e/topicmaps/x-and-ride/src/e2e/smoke.spec.ts
@@ -1,23 +1,27 @@
-import { test } from "@playwright/test";
-import { runMapSmokeTest, setupSmokeTest, setupAllMocks } from "@carma-commons/e2e";
+import { expect, test } from "@playwright/test";
+// import { runMapSmokeTest, setupSmokeTest, setupAllMocks } from "@carma-commons/e2e";
 
 test.describe("x-and-ride smoke test", () => {
-  test.beforeEach(async ({ context, page }) => {
-    await setupAllMocks(context, ["bezirke", "quartiere", 'kitas', "pois", "prbr", "prbr.data"]);
-    await setupSmokeTest(page, "/", {
-      navigationTimeout: 30000,
-      waitForNetworkIdle: true,
-    });
-  });
+  // test.beforeEach(async ({ context, page }) => {
+  //   await setupAllMocks(context, ["bezirke", "quartiere", 'kitas', "pois", "prbr", "prbr.data"]);
+  //   await setupSmokeTest(page, "/", {
+  //     navigationTimeout: 30000,
+  //     waitForNetworkIdle: true,
+  //   });
+  // });
 
   test("map loads with key controls", async ({ page }) => {
     // Run the comprehensive smoke test from the shared library
-    await runMapSmokeTest(page, {
-      fuzzySearchTimeout: 10000,
-      checkZoomControl: true,
-      checkFuzzySearch: true,
-      checkApplicationMenu: true,
-      checkInfoBox: true,
-    });
+    // await runMapSmokeTest(page, {
+    //   fuzzySearchTimeout: 10000,
+    //   checkZoomControl: true,
+    //   checkFuzzySearch: true,
+    //   checkApplicationMenu: true,
+    //   checkInfoBox: true,
+    // });
+
+    await page.goto("/");
+    await page.waitForLoadState("networkidle");
+    await expect(page.getByText('Test')).toBeVisible();
   });
 });

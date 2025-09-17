8478086b46bd6b2177056675321e9866b7452c94
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-09-17 13:53:44 +0200

    change cache keys

--- libraries/mapping/map-controls-layout/src/lib/map-control.tsx
+++ libraries/mapping/map-controls-layout/src/lib/map-control.tsx
@@ -16,6 +16,7 @@ export type ControlComponent = {
 };
 
 
+
 interface ControlContextType {
   addControl: (component: ControlComponent) => void;
   removeControl: (component: ControlComponent) => void;

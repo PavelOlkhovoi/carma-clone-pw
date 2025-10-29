8d25677d9ba5bc3f917e207aee18485ae32112b9
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-10-29 16:16:27 +0100

    force all tests

--- libraries/mapping/fuzzy-search/src/lib/lib-fuzzy-search.tsx
+++ libraries/mapping/fuzzy-search/src/lib/lib-fuzzy-search.tsx
@@ -91,7 +91,7 @@ export function LibFuzzySearch({
   const inputStyle = {
     width: "calc(100% - 32px)",
     borderTopLeftRadius: 0,
-    // fontSize: "14px",
+    fontSize: "14px",
   };
   const btnClosRef = useRef<HTMLButtonElement>(null);
   const autoCompleteRef = useRef<BaseSelectRef | null>(null);

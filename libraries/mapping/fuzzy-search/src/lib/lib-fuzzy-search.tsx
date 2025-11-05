fa75659957296e51fc08b488c40c622e1b6e1bff
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-11-05 10:22:13 +0100

    add check test step

--- libraries/mapping/fuzzy-search/src/lib/lib-fuzzy-search.tsx
+++ libraries/mapping/fuzzy-search/src/lib/lib-fuzzy-search.tsx
@@ -92,7 +92,7 @@ export function LibFuzzySearch({
 
   const inputStyle = {
     width: "calc(100% - 32px)",
-    borderTopLeftRadius: 0,
+    // borderTopLeftRadius: 0,
     fontSize: "14px",
   };
   const btnClosRef = useRef<HTMLButtonElement>(null);

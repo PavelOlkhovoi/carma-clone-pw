81f9d26401223709128c2e95cc67904a6f0b1ba5
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-11-24 18:56:31 +0100

    add html reporter for lagis and geoportal

--- e2e/lagis/lagis-desktop/playwright.config.ts
+++ e2e/lagis/lagis-desktop/playwright.config.ts
@@ -9,12 +9,13 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: "html",
+  reporter: "github",
   use: {
     baseURL: "http://localhost:4222",
     trace: "on-first-retry",
     screenshot: "only-on-failure",
-        navigationTimeout: 15_000, actionTimeout: 10_000,
+    navigationTimeout: 15_000,
+    actionTimeout: 10_000,
   },
   projects: [
     {

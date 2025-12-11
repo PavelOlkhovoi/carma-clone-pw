8442061c1d2bf5c775e8c33d5a7a872ae16e4f47
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-11 17:54:12 +0100

    updated submodule

--- apps/geoportal/src/app/App.tsx
+++ apps/geoportal/src/app/App.tsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-// 3rd party Modules
 import { Button, Modal } from "antd";
 import { ErrorBoundary } from "react-error-boundary";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";

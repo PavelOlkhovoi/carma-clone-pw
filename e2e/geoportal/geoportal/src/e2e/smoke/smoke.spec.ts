6f0479440148882c27f420fbacb9df5dcec37f20
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-12 10:08:09 +0100

    change potential areas smoke test

--- e2e/geoportal/geoportal/src/e2e/smoke/smoke.spec.ts
+++ e2e/geoportal/geoportal/src/e2e/smoke/smoke.spec.ts
@@ -1,53 +1,137 @@
-import { setupAllMocks } from "@carma-commons/e2e";
 import { test, expect } from "@playwright/test";
+import {
+  runMapSmokeTest,
+  setupSmokeTest,
+  setupAllMocks,
+} from "@carma-commons/e2e";
 
-test.describe("geoportal smoke test", () => {
-  test.beforeEach(async ({ context, page }) => {
-    await setupAllMocks(context);
+test.describe("potenzialflaechen-online smoke test", () => {
+  test("map loads with key controls", async ({ context, page }) => {
+    await setupAllMocks(context, ["bezirke", "quartiere", "kitas", "pois"]);
+
+    // Mock map style JSON files
+    const mockStyleJson = {
+      version: 8,
+      name: "Mock Style",
+      sources: {},
+      layers: [],
+    };
+
+    await context.route(
+      "https://omt.map-hosting.de/styles/cismet-light/style.json",
+      (route) =>
+        route.fulfill({
+          status: 200,
+          contentType: "application/json",
+          body: JSON.stringify(mockStyleJson),
+        })
+    );
+
+    await context.route(
+      "https://omt.map-hosting.de/styles/osm-bright-grey/style.json",
+      (route) =>
+        route.fulfill({
+          status: 200,
+          contentType: "application/json",
+          body: JSON.stringify(mockStyleJson),
+        })
+    );
+
+    await context.route(
+      "https://omt.map-hosting.de/styles/brunnen/style.json",
+      (route) =>
+        route.fulfill({
+          status: 200,
+          contentType: "application/json",
+          body: JSON.stringify(mockStyleJson),
+        })
+    );
+
+    await context.route(
+      "https://omt.map-hosting.de/styles/kanal/style.json",
+      (route) =>
+        route.fulfill({
+          status: 200,
+          contentType: "application/json",
+          body: JSON.stringify(mockStyleJson),
+        })
+    );
+
+    await context.route(
+      "https://omt.map-hosting.de/styles/gewaesser/style.json",
+      (route) =>
+        route.fulfill({
+          status: 200,
+          contentType: "application/json",
+          body: JSON.stringify(mockStyleJson),
+        })
+    );
+
+    await context.route(
+      " https://potenzialflaechen-online-api.cismet.de/actions/WUNDA_BLAU.dataAquisition/tasks?resultingInstanceType=result",
+      (route) =>
+        route.fulfill({
+          status: 200,
+          contentType: "application/json",
+          body: "[]",
+        })
+    );
+
+    await context.route(
+      "https://offline-data.cismet.de/offline-data/*",
+      (route) =>
+        route.fulfill({
+          status: 200,
+          contentType: "application/json",
+          body: "[]",
+        })
+    );
+
+    await context.route(
+      "https://potenzialflaechen-online-api.cismet.de/users",
+      (route) =>
+        route.fulfill({
+          status: 200,
+          contentType: "application/json",
+          body: JSON.stringify({
+            user: "cismetTest",
+            domain: "WUNDA_BLAU",
+            jwt: "0000000",
+            passHash: "0000000",
+            userGroups: [
+              "_Potenzialflaechen_LESEN",
+              "_Potenzialflaechen_LESEN_EXT",
+              "_Potenzialflaechen_PUBLIC",
+              "_Umweltalarm",
+            ],
+          }),
+        })
+    );
+
     await page.goto("/");
-    await page.waitForLoadState("networkidle");
-  });
+    await page.addStyleTag({
+      content: `
+      * { transition: none !important; animation: none !important; }
+      .collapse { display: block !important; height: auto !important; }
+    `,
+    });
 
-  test("Map loads with key controls and buttons", async ({ page }) => {
-    await expect(
-      page.locator('[data-test-id="zoom-in-control"]')
-    ).toBeVisible();
-    await expect(page.locator('[data-test-id="home-control"]')).toBeVisible();
-    await expect(
-      page.locator('[data-test-id="measurement-control"]')
-    ).toBeVisible();
-    await expect(page.locator('[data-test-id="3d-control"]')).toBeVisible();
+    const modal = page.locator(".modal-content");
+    await expect(modal).toBeVisible();
+    await page
+      .getByRole("textbox", { name: "WuNDa Benutzername" })
+      .fill("cismetTest");
+    await page.getByRole("textbox", { name: "Passwort" }).fill("cismetTest");
+    await page.getByRole("button", { name: "Anmeldung" }).click();
 
-    await expect(page.locator('[data-test-id="2d-control"]')).toHaveCount(0);
+    await expect(modal).toHaveCount(0);
 
-    await expect(
-      page.locator('[data-test-id="compass-control"]')
-    ).toBeVisible();
-    await expect(
-      page.locator('[data-test-id="feature-info-control"]')
-    ).toBeVisible();
-    await expect(
-      page.locator('[data-test-id="helper-overlay-btn"]')
-    ).toBeVisible();
-    await expect(page.locator('[data-test-id="reload-btn"]')).toBeVisible();
-    await expect(
-      page.locator('[data-test-id="kartenebenen-hinzufügen-btn"]')
-    ).toBeVisible();
-    await expect(
-      page.locator('[data-test-id="hintergrundkarte-btn"]')
-    ).toBeVisible();
-    await expect(page.locator('[data-test-id="speichern-btn"]')).toBeVisible();
-    await expect(page.locator('[data-test-id="teilen-btn"]')).toBeVisible();
-    await expect(page.locator('[data-test-id="fuzzy-search"]')).toBeVisible();
-
-    await expect(page.locator("#cmdCloseModalApplicationMenu")).toHaveCount(0);
-
-    // Open modal
-    await page.locator('[data-test-id="modal-menu-btn"]').click();
-    await expect(page.locator("#cmdCloseModalApplicationMenu")).toBeVisible();
-
-    // Close modal
-    await page.locator("#cmdCloseModalApplicationMenu").click();
-    await expect(page.locator("#cmdCloseModalApplicationMenu")).toHaveCount(0);
+    await runMapSmokeTest(page, {
+      fuzzySearchTimeout: 10000,
+      checkZoomControl: true,
+      checkFuzzySearch: true,
+      checkApplicationMenu: true,
+      checkInfoBox: true,
+    });
   });
 });

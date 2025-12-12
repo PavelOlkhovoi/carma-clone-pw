82a74305edf5f58f116fec785fa19d51c4d5efce
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-12 11:57:23 +0100

    add lagis smoke test

--- libraries/mapping/fuzzy-search/src/lib/lib-fuzzy-search.tsx
+++ libraries/mapping/fuzzy-search/src/lib/lib-fuzzy-search.tsx
@@ -7,6 +7,8 @@ import { faLocationDot, faTimes } from "@fortawesome/free-solid-svg-icons";
 import type { BaseSelectRef } from "rc-select";
 
 // import IconComp from "react-cismap/commons/Icon";
+// import IconComp from "react-cismap/commons/Icon";
+// import IconComp from "react-cismap/commons/Icon";
 
 import {
   generateOptions,

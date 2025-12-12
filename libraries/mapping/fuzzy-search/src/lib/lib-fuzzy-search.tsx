815ba7106f475e774e623b33c1adb6a008bd5d3d
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-12-12 09:41:25 +0100

    change the potential areas online folder

--- libraries/mapping/fuzzy-search/src/lib/lib-fuzzy-search.tsx
+++ libraries/mapping/fuzzy-search/src/lib/lib-fuzzy-search.tsx
@@ -7,11 +7,6 @@ import { faLocationDot, faTimes } from "@fortawesome/free-solid-svg-icons";
 import type { BaseSelectRef } from "rc-select";
 
 // import IconComp from "react-cismap/commons/Icon";
-// import IconComp from "react-cismap/commons/Icon";
-// import IconComp from "react-cismap/commons/Icon";
-// import IconComp from "react-cismap/commons/Icon";
-// import IconComp from "react-cismap/commons/Icon";
-// import IconComp from "react-cismap/commons/Icon";
 
 import {
   generateOptions,
